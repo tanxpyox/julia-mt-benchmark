ee73bf49e56e9fbcb7bacf4c29c9ceba1ff038a6
亶父 (taan² fu²) <tanxpyox@gmail.com>
2021-05-04 00:49:48 +0800

    Update ThreeNPlusOne.jl

--- ThreeNPlusOne.jl
+++ ThreeNPlusOne.jl
@@ -1,7 +1,9 @@
+using ProgressMeter
+
 println("Single thread:...")
 @time begin 
-  a = collect(1:100000)
-  for i in 1:length(a)
+  a = collect(1:1000000)
+  @showprogress for i in 1:length(a)
     while a[i] != 1 
       if a[i] % 2 == 0
         a[i] = a[i] / 2
@@ -14,8 +16,8 @@ end
 
 println("Multithread:...")
 @time begin 
-  a = collect(1:100000)
-  Threads.@threads for i in 1:length(a)
+  a = collect(1:1000000)
+  Threads.@threads @showprogress for i in 1:length(a)
     while a[i] != 1 
       if a[i] % 2 == 0
         a[i] = a[i] / 2
